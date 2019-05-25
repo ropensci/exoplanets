f1befec63d42b2834906c5f3b330dcd3058a8bce
tylurp <tylurp1@gmail.com>
2019-05-25 12:42:15 -0700

    speed up tests, select 1 column from random dataset :hourglass:

--- tests/testthat/test-exoplanets.R
+++ tests/testthat/test-exoplanets.R
@@ -1,13 +1,17 @@
 test_that("multiplication works", {
   # This test grabs a random table (excluding time series tables because
   # they are large) and confirms that a data.frame was successfully created.
-  # Some datasets are larger than others and so this test can take awhile,
-  # longest at the moment was 31 seconds. Maybe using httr would be helpful to
-  # make the test quicker.
+  # For speed, only the first column is taken.
+
+  # set up
   non_time_series <- names(tbls)[-which(grepl("timeseries", names(tbls)))]
   random_tbl <- sample(non_time_series, 1)
-  exo_output <- class(exo(random_tbl))
-  exo_raw_output <- class(exo_raw("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets"))
+  random_tbl_cols <- exo(paste0(random_tbl, "&getDefaultColumns"))
+  test <- paste0(random_tbl, "&select=", names(random_tbl_cols[1]))
+  exo_output <- class(exo(test))
+  exo_raw_output <- class(exo_raw(paste0("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=", test)))
+
+  # tests
   expect_equal(exo_output, "data.frame")
   expect_equal(class(tbls), "list")
   expect_equal(exo_raw_output, "data.frame")
