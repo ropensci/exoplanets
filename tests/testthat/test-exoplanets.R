91e2902b7da7c9d5d11f3a2cdca40333eb1ad14d
tylurp <tylurp1@gmail.com>
2019-05-26 08:13:06 -0700

    more tests :beers:

--- tests/testthat/test-exoplanets.R
+++ tests/testthat/test-exoplanets.R
@@ -6,7 +6,7 @@ test_that("exoplanets works", {
   # set up
   non_time_series <- names(tbls)[-which(grepl("timeseries", names(tbls)))]
   random_tbl <- sample(non_time_series, 1)
-  random_tbl_cols <- exo(paste0(random_tbl, "&getDefaultColumns"))
+  random_tbl_cols <- exo_column_names(random_tbl, cols = "default")
   test <- paste0(random_tbl, "&select=", names(random_tbl_cols[1]))
   exo_output <- class(exo(test))
   exo_raw_output <- class(exo_raw(paste0("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=", test)))
@@ -17,8 +17,10 @@ test_that("exoplanets works", {
   expect_equal(exo_output, "data.frame")
   expect_equal(class(tbls), "list")
   expect_equal(exo_raw_output, "data.frame")
-  expect_error(exo_raw())
+  expect_error(exo_raw(NULL))
   expect_equal(class(exo_cols_default), "character")
   expect_true(length(exo_cols_default) > 1)
   expect_true(length(exo_cols_default) < length(exo_cols_all))
+  expect_equal(class(exo("cumulative", "all")), "data.frame")
+  expect_equal(ncol(exo("cumulative", "kepid")), 1)
 })
