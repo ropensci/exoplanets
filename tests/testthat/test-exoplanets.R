75fc0f5335186292c06b872dd3610c7314fafb2a
tylurp <tylurp1@gmail.com>
2019-05-26 07:00:41 -0700

    update tests :memo:

--- tests/testthat/test-exoplanets.R
+++ tests/testthat/test-exoplanets.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("exoplanets works", {
   # This test grabs a random table (excluding time series tables because
   # they are large) and confirms that a data.frame was successfully created.
   # For speed, only the first column is taken.
@@ -10,10 +10,15 @@ test_that("multiplication works", {
   test <- paste0(random_tbl, "&select=", names(random_tbl_cols[1]))
   exo_output <- class(exo(test))
   exo_raw_output <- class(exo_raw(paste0("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=", test)))
+  exo_cols_default <- exo_column_names()
+  exo_cols_all <- exo_column_names(cols = "all")
 
   # tests
   expect_equal(exo_output, "data.frame")
   expect_equal(class(tbls), "list")
   expect_equal(exo_raw_output, "data.frame")
   expect_error(exo_raw())
+  expect_equal(class(exo_cols_default), "character")
+  expect_true(length(exo_cols_default) > 1)
+  expect_true(length(exo_cols_default) < length(exo_cols_all))
 })
